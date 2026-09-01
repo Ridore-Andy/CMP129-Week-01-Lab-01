52b02fe7cc4ca65cc062e1c40a26b72e5fa859b4
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-01 14:38:24 -0400

    message

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,4 +1,22 @@
-public class BoxOffice {
-    
+import java.util.Scanner;
+
+public class BoxOffice 
+{
+    public static void main(String[] args) 
+    {
+        java.util.Scanner keyboard;
+        
+
+        double adultTicket = 10.00;
+        double childTicket = 6.00;
+
+        System.out.println(adultTicket);
+        System.out.println(childTicket);
+
+        System.out.println("What is the name of the movie? ");
+
+        
+
+    }
     
 }
