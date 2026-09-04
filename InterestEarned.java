94c2a2ca603c29ef017eedc9c4640164e55f884f
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-04 14:29:19 -0400

    Implement compound interest calculation
    Added functionality to calculate compound interest based on user input.

--- InterestEarned.java
+++ InterestEarned.java
@@ -1,3 +1,26 @@
-public class InterestEarned {
+import java.util.Scanner;
+import java.lang.Math;
+
+public class InterestEarned 
+{
+    public static void main(String[] args) 
+    {
+        Scanner keyboard = new Scanner(System.in);
+        //
+
+        System.out.println("What is the Principal?");
+        double Principal = keyboard.nextDouble();
+
+        System.out.println("\nWhat is the interest rate?");
+        double interestRate = keyboard.nextDouble();
+
+        System.out.println("\nHow many times has the interest been compounded?");
+        int timesCompund = keyboard.nextInt();
+
+        double Amount = Principal * Math.pow(1+interestRate/timesCompund, timesCompund);
+        System.out.println(Amount);
+
+        keyboard.close();
+    }
     
 }
