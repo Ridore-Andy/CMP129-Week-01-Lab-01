44e1029f9f99c7851ce272a27153c992fa2aa9d8
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-04 22:51:13 -0400

    Enhance InterestEarned to display interest details
    Added calculation for interest and formatted output.

--- InterestEarned.java
+++ InterestEarned.java
@@ -13,12 +13,19 @@ public class InterestEarned
 
         System.out.println("\nWhat is the interest rate?");
         double interestRate = keyboard.nextDouble();
+        double ratePercent = interestRate*100;
 
         System.out.println("\nHow many times has the interest been compounded?");
         int timesCompund = keyboard.nextInt();
 
         double Amount = Principal * Math.pow(1+interestRate/timesCompund, timesCompund);
-        System.out.println(Amount);
+        double Interest = Amount - Principal;
+
+        System.out.printf("\nInterest Rate: %.2f%%", ratePercent);
+        System.out.printf("\nTimes Compunded: %d", timesCompund);
+        System.out.printf("\nPrincipal: $%.2f", Principal);
+        System.out.printf("\nInterest: $%.2f", Interest);
+        System.out.printf("\nAmount in Savings: $%.2f", Amount);
 
         keyboard.close();
     }
